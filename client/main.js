6be63643900815c5502c827fd7ec13cdcb632d5a
danielpoehle <d.poehle@gmx.de>
2018-01-13 18:16:21 +0100

    created app with CRUD

--- client/main.js
+++ client/main.js
@@ -1,3 +1,40 @@
 import { Template } from 'meteor/templating';
+import { Notes } from '../lib/collections.js';
 
 import './main.html';
+
+Template.body.helpers({
+  notes(){
+    return Notes.find({});
+  }
+});
+
+Template.note.events({
+  'click .delete-note': function(){
+    Notes.remove(this._id);
+  }
+});
+
+Template.add.events({
+  'submit .add-form': function(event, template){
+    event.preventDefault();
+
+    //Get input value
+    const target = event.target;
+    const textValue = target.newNote.value;
+
+    //Insert Note into collection
+    Notes.insert({
+      text: textValue,
+      createdAt: new Date(),
+      user: 01
+    });
+
+    //Clear the form
+    target.newNote.value = "";
+
+    //Close the modal
+    $('#addNote').modal('close');
+
+  }
+});
